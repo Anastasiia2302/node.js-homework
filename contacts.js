632fd24aca534760ae8f22fef649ed6beb448324
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-05-19 11:27:16 +0300

    start

---
+++ contacts.js
@@ -0,0 +1,22 @@
+
+
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+function listContacts() {
+    
+  }
+  
+  function getContactById(contactId) {
+   
+  }
+  
+  function removeContact(contactId) {
+    
+  }
+  
+  function addContact(name, email, phone) {
+    
+  }
